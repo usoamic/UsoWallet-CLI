34d54fd2a2861becc1498d8a78a15cdb52fa5573
bbusacs478 <a2uso@tuta.io>
2019-07-20 23:10:18 -0700

    Fix typo and add validateMnemonicPhrase() function

--- src/main/kotlin/io/usoamic/cli/util/ValidateUtil.kt
+++ src/main/kotlin/io/usoamic/cli/util/ValidateUtil.kt
@@ -1,6 +1,7 @@
 package io.usoamic.cli.util
 
 import io.usoamic.cli.exception.ValidateUtilException
+import io.usoamic.usoamickotlin.exception.InvalidMnemonicPhraseException
 
 class ValidateUtil {
     companion object {
@@ -8,8 +9,12 @@ class ValidateUtil {
             validateThatNotEmpty(password, "Password Required")
         }
 
-        fun validatePrivateKey(password: String) {
-            validateThatNotEmpty(password, "Private Key Required")
+        fun validatePrivateKey(privateKey: String) {
+            validateThatNotEmpty(privateKey, "Private Key Required")
+        }
+
+        fun validateMnemonicPhrase(mnemonicPhrase: String) {
+            validateThatNotEmpty(mnemonicPhrase, "Mnemonic Phrase Required")
         }
 
         private fun validateThatNotEmpty(str: String, message: String) {
