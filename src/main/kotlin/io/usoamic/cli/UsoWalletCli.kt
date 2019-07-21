4e59fdfef27ab34515c97a057a8cbf31e44cd5ca
bbusacs478 <a2uso@tuta.io>
2019-07-21 00:08:03 -0700

    Implement eth_transfer, burn and get_transaction

--- src/main/kotlin/io/usoamic/cli/UsoWalletCli.kt
+++ src/main/kotlin/io/usoamic/cli/UsoWalletCli.kt
@@ -6,6 +6,9 @@ import io.usoamic.cli.util.getOrZero
 import io.usoamic.usoamickotlin.core.Usoamic
 import io.usoamic.usoamickotlin.exception.InvalidMnemonicPhraseException
 import io.usoamic.usoamickotlin.exception.InvalidPrivateKeyException
+import io.usoamic.usoamickotlin.util.Coin
+import org.web3j.utils.Convert
+import java.math.BigInteger
 import java.util.*
 import javax.inject.Inject
 
@@ -47,13 +50,24 @@ class UsoWalletCli {
                     println(usoamic.address)
                 }
                 "get_eth_balance" -> {
-                    println(usoamic.getEthBalance())
+                    val weiBalance = usoamic.getEthBalance()
+                    val ethBalance = Convert.fromWei(weiBalance.toBigDecimal(), Convert.Unit.ETHER)
+                    println(ethBalance.toPlainString())
                 }
                 "get_uso_balance" -> {
-                    println(usoamic.getUsoBalance())
+                    val satBalance = usoamic.getUsoBalance()
+                    val coinBalance = Coin.fromSat(satBalance!!).toBigDecimal()
+                    println(coinBalance.toPlainString())
                 }
                 "eth_transfer" -> {
-
+                    val password = args.getOrEmpty(1)
+                    val to = args.getOrEmpty(2)
+                    val value = args.getOrZero(3)
+                    ValidateUtil.validatePassword(password)
+                    ValidateUtil.validateAddress(to)
+                    ValidateUtil.validateTransferValue(value)
+                    val txHash = usoamic.transferEther(password, to, value.toBigInteger())
+                    println(txHash)
                 }
                 "uso_transfer" -> {
                     val password = args.getOrEmpty(1)
@@ -62,16 +76,26 @@ class UsoWalletCli {
                     ValidateUtil.validatePassword(password)
                     ValidateUtil.validateAddress(to)
                     ValidateUtil.validateTransferValue(value)
-                    usoamic.transfer(password, to, value.toBigInteger())
+                    val txHash = usoamic.transfer(password, to, value.toBigInteger())
+                    println(txHash)
                 }
                 "burn" -> {
-
+                    val password = args.getOrEmpty(1)
+                    val value = args.getOrZero(3)
+                    ValidateUtil.validatePassword(password)
+                    ValidateUtil.validateTransferValue(value)
+                    val txHash = usoamic.burn(password, value.toBigInteger())
+                    println(txHash)
                 }
                 "get_transaction" -> {
-
+                    val txId = args.getOrEmpty(1)
+                    ValidateUtil.validateTxId(txId)
+                    val transaction = usoamic.getTransaction(BigInteger.ONE)
+                    println(transaction)
                 }
             }
         } catch (e: Exception) {
+            e.printStackTrace()
             when (e) {
                 is InvalidMnemonicPhraseException -> {
                     println("Invalid Mnemonic Phrase")
