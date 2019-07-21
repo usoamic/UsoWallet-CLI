e9a758c1b72cbcd0298a6d86d9baa8b6838009f7
bbusacs478 <a2uso@tuta.io>
2019-07-21 01:57:02 -0700

    Add apply to all functions

--- src/main/kotlin/io/usoamic/cli/util/ValidateUtil.kt
+++ src/main/kotlin/io/usoamic/cli/util/ValidateUtil.kt
@@ -7,46 +7,46 @@ import java.math.BigInteger
 
 class ValidateUtil {
     companion object {
-        fun validatePassword(password: String) {
+        fun validatePassword(password: String) = apply {
             validateThatNotEmpty(password, "Password Required")
         }
 
-        fun validatePrivateKey(privateKey: String) {
+        fun validatePrivateKey(privateKey: String) = apply {
             validateThatNotEmpty(privateKey, "Private Key Required")
         }
 
-        fun validateMnemonicPhrase(mnemonicPhrase: String) {
+        fun validateMnemonicPhrase(mnemonicPhrase: String) = apply {
             validateThatNotEmpty(mnemonicPhrase, "Mnemonic Phrase Required")
         }
 
-        fun validateAddress(address: String) {
+        fun validateAddress(address: String) = apply {
             validateThatNotEmpty(address, "Address Required")
             if(!WalletUtils.isValidAddress(address)) {
                 throw ValidateUtilException("Invalid Address")
             }
         }
 
-        fun validateTransferValue(value: String) {
+        fun validateTransferValue(value: String) = apply {
             val intValue = value.toBigIntegerOrNull() ?: throw ValidateUtilException("Value Required")
             if(intValue > BigInteger.ZERO) {
                 throw ValidateUtilException("Invalid Value")
             }
         }
 
-        fun validateId(id: String) {
+        fun validateId(id: String) = apply {
             val intId = id.toBigIntegerOrNull() ?: throw ValidateUtilException("Invalid Id")
             if(intId < BigInteger.ZERO) {
                 throw ValidateUtilException("Id must be greater than or equal to zero")
             }
         }
 
-        fun validateIds(vararg ids: String) {
+        fun validateIds(vararg ids: String) = apply {
             for(id in ids) {
                 validateId(id)
             }
         }
 
-        private fun validateThatNotEmpty(str: String, message: String) {
+        private fun validateThatNotEmpty(str: String, message: String) = apply {
             if(str.isEmpty()) {
                 throw ValidateUtilException(message)
             }
