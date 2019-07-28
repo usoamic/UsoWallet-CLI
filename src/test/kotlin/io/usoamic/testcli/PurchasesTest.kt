974089626a7f4ea62a26f323d7c0c3732229f263
bbusacs478 <a2uso@tuta.io>
2019-07-28 11:14:27 -0700

    Add test for getPurchaseByAddress() and getNumberOfPurchasesByAddress()

--- src/test/kotlin/io/usoamic/testcli/PurchasesTest.kt
+++ src/test/kotlin/io/usoamic/testcli/PurchasesTest.kt
@@ -1,10 +1,13 @@
 package io.usoamic.testcli
 
 import io.usoamic.cli.Core
+import io.usoamic.cli.exception.ObjectNotFoundException
 import io.usoamic.testcli.other.TestConfig
 import io.usoamic.usoamickotlin.core.Usoamic
 import io.usoamic.usoamickotlin.util.Coin
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.math.BigInteger
 import javax.inject.Inject
 import kotlin.random.Random
 
@@ -18,22 +21,42 @@ class PurchasesTest {
 
     @Test
     fun makePurchaseTest() {
-        val address = core.getResponse("get_address")
-
         val appId = "appId" + Random.nextInt()
         val purchaseId = "purchaseId" + Random.nextInt()
         val cost = Coin.ONE.toSat()
 
-        val numberOfPurchases = core.getResponse("get_number_of_purchases_by_address $address")
+        val numberOfPurchases = core.getResponse("get_number_of_purchases_by_address ${TestConfig.DEFAULT_ADDRESS}")
         val txHash = core.getResponse("make_purchase ${TestConfig.PASSWORD} $appId $purchaseId $cost")
 
         usoamic.waitTransactionReceipt(txHash) {
-            val purchase = core.getResponse("get_purchase_by_address $address $numberOfPurchases")
+            val purchase = core.getResponse("get_purchase_by_address ${TestConfig.DEFAULT_ADDRESS} $numberOfPurchases")
             assert(purchase.contains("isExist=true"))
             assert(purchase.contains("appId=$appId"))
             assert(purchase.contains("id=$numberOfPurchases"))
             assert(purchase.contains("purchaseId=$purchaseId"))
             assert(purchase.contains("cost=$cost"))
         }
+    }
+
+    @Test
+    fun getPurchaseByAddressTest() {
+        val id = BigInteger.ZERO!!
+        val numberOfPurchases = core.getResponse("get_number_of_purchases_by_address ${TestConfig.DEFAULT_ADDRESS}").toBigInteger()
+        val isExist = numberOfPurchases > id
+
+        if(isExist) {
+            val purchase = core.getResponse("get_purchase_by_address ${TestConfig.DEFAULT_ADDRESS} $id")
+            assert(purchase.contains("id=$id"))
+        }
+
+        assertThrows<ObjectNotFoundException> {
+            core.getResponse("get_purchase_by_address ${TestConfig.DEFAULT_ADDRESS} $numberOfPurchases")
+        }
+    }
+
+    @Test
+    fun getNumberOfPurchasesByAddressTest() {
+        val result = core.getResponse("get_number_of_purchases_by_address ${TestConfig.DEFAULT_ADDRESS}")
+        assert(result.toBigInteger() >= BigInteger.ZERO)
     }
 }
