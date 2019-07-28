c54b934916242eb73c4d48a71c14f4076973a0dc
bbusacs478 <a2uso@tuta.io>
2019-07-28 11:32:44 -0700

    Implement tests for Owner class

--- src/test/kotlin/io/usoamic/testcli/OwnerTest.kt
+++ src/test/kotlin/io/usoamic/testcli/OwnerTest.kt
@@ -1,6 +1,12 @@
 package io.usoamic.testcli
 
 import io.usoamic.cli.Core
+import io.usoamic.testcli.other.TestConfig
+import io.usoamic.testcli.util.Utils
+import io.usoamic.testcli.util.Utils.Companion.getNewAddress
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.web3j.exceptions.MessageDecodingException
 import javax.inject.Inject
 
 class OwnerTest {
@@ -8,5 +14,20 @@ class OwnerTest {
 
     init {
         BaseUnitTest.componentTest.inject(this)
+    }
+
+    @Test
+    fun setFrozenTest() {
+        assertThrows<MessageDecodingException> {
+            core.getResponse("set_frozen ${TestConfig.PASSWORD} true")
+        }
+    }
+
+    @Test
+    fun setOwnerTest() {
+        assertThrows<MessageDecodingException> {
+            val address = getNewAddress()
+            core.getResponse("set_owner ${TestConfig.PASSWORD} $address")
+        }
     }
 }
