bc2ce6e18226f59854ad1e61006102659f137e5f
bbusacs478 <a2uso@tuta.io>
2019-07-28 10:53:59 -0700

    Fix DEFAULT_ADDRESS

--- src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
+++ src/test/kotlin/io/usoamic/testcli/other/TestConfig.kt
@@ -7,7 +7,7 @@ class TestConfig {
         const val NODE: String = "https://rinkeby.infura.io:443"
         const val ACCOUNT_FILENAME: String = "test_account.json"
         const val CONTRACT_ADDRESS: String = Config.CONTRACT_ADDRESS
-        const val DEFAULT_ADDRESS: String = "0xb894d4329329b6150258c0048f6dfb41257359f5"
+        const val DEFAULT_ADDRESS: String = "0x8b27fa2987630a1acd8d868ba84b2928de737bc2"
         const val OWNER_ADDRESS: String = "0x5d8766ac0075bdf81b48f0bfcf92449e9def0f37"
         const val PASSWORD: String = "1234!"
         const val VERSION: String = "v2"
