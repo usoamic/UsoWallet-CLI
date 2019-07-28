21a9b11d36ecc6321027efa8e7a1879bd0e9247a
bbusacs478 <a2uso@tuta.io>
2019-07-28 01:27:22 -0700

    Changes in di

--- src/main/kotlin/io/usoamic/cli/di/AppComponent.kt
+++ src/main/kotlin/io/usoamic/cli/di/AppComponent.kt
@@ -10,13 +10,4 @@ import javax.inject.Singleton
 @Component(modules = [UsoamicModule::class])
 interface AppComponent {
     fun inject(clazz: UsoWalletCli)
-    fun inject(clazz: Ideas)
-    fun inject(clazz: Notes)
-    fun inject(clazz: Owner)
-    fun inject(clazz: Purchases)
-    fun inject(clazz: Swap)
-    fun inject(clazz: TransactionExplorer)
-    fun inject(clazz: Usoamic)
-    fun inject(clazz: AccountManager)
-    fun inject(clazz: Core)
 }
