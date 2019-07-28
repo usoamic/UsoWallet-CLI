52ec4fb90a1dbd8ef594faf0e744deb3fd51b25a
bbusacs478 <a2uso@tuta.io>
2019-07-28 12:07:17 -0700

    Inject dependency to IdeasTest

--- src/test/kotlin/io/usoamic/testcli/IdeasTest.kt
+++ src/test/kotlin/io/usoamic/testcli/IdeasTest.kt
@@ -1,10 +1,12 @@
 package io.usoamic.testcli
 
 import io.usoamic.cli.Core
+import io.usoamic.usoamickotlin.core.Usoamic
 import javax.inject.Inject
 
 class IdeasTest {
     @Inject lateinit var core: Core
+    @Inject lateinit var usoamic: Usoamic
 
     init {
         BaseUnitTest.componentTest.inject(this)
