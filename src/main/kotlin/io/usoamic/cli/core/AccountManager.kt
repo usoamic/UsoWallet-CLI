1ca39e2554477c5cbf3f3922ba4edb1bbcafedf5
bbusacs478 <a2uso@tuta.io>
2019-08-03 05:02:30 -0700

    Add functions for creating private key and mnemonic phrase

--- src/main/kotlin/io/usoamic/cli/core/AccountManager.kt
+++ src/main/kotlin/io/usoamic/cli/core/AccountManager.kt
@@ -3,6 +3,10 @@ package io.usoamic.cli.core
 import io.usoamic.cli.util.ValidateUtil
 import io.usoamic.cli.util.getOrEmpty
 import io.usoamic.usoamickotlin.core.Usoamic
+import io.usoamic.usoamickotlin.util.MnemonicUtils
+import org.web3j.crypto.Credentials
+import org.web3j.crypto.Keys
+import org.web3j.crypto.WalletUtils
 import javax.inject.Inject
 
 class AccountManager @Inject constructor(private val usoamic: Usoamic) {
@@ -24,5 +28,18 @@ class AccountManager @Inject constructor(private val usoamic: Usoamic) {
             .validatePassword(password)
 
         return usoamic.importMnemonic(password, privateKey)
+    }
+
+    fun createPrivateKey(): String {
+        var privateKey: String
+        do {
+            privateKey = Credentials.create(Keys.createEcKeyPair()).ecKeyPair.privateKey.toString(16)
+        }
+        while (!WalletUtils.isValidPrivateKey(privateKey))
+        return privateKey
+    }
+
+    fun createMnemonicPhrase(): String {
+        return MnemonicUtils.generateMnemonic()
     }
 }
