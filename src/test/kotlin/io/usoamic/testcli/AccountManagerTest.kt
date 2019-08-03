061dd63f49f7abb341aa3d3042d3ababceb2412e
bbusacs478 <a2uso@tuta.io>
2019-08-03 04:24:24 -0700

    Inject dependency to test

--- src/test/kotlin/io/usoamic/testcli/AccountManagerTest.kt
+++ src/test/kotlin/io/usoamic/testcli/AccountManagerTest.kt
@@ -1,4 +1,13 @@
 package io.usoamic.testcli
 
+import io.usoamic.cli.Core
+import javax.inject.Inject
+
 class AccountManagerTest {
+    @Inject
+    lateinit var core: Core
+
+    init {
+        BaseUnitTest.componentTest.inject(this)
+    }
 }
